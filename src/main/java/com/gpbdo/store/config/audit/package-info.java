9e31a32c433fa6528c0761364b4017bdc110bc30
shakeys <shakeys@Shakeys>
2018-03-19 14:53:10 +0800

    Generate store application

---
+++ src/main/java/com/gpbdo/store/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.gpbdo.store.config.audit;
---
+++ src/main/java/com/gpbdo/store/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.gpbdo.store.web.rest.vm;
